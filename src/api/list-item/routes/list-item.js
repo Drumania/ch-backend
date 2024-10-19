ec4b8386a4e76804f3989582ae19610df20d5bb9
martinbrumana <martinbrumana@gmail.com>
2024-10-19 15:23:23 -0300

    init strapi

---
+++ src/api/list-item/routes/list-item.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * list-item router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::list-item.list-item');
--- src/api/list/routes/list.js
+++ src/api/list/routes/list.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * article router.
+ * list router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::article.article');
+module.exports = createCoreRouter('api::list.list');
